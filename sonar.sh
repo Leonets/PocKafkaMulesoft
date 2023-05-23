a91e2e2d598f9c0cef61dbab7bc6303237c466b9
lbattagli@florence-consulting.it <lbattagli@florence-consulting.it>
2023-05-23 11:40:11 +0200

    lbattagli | update project

---
+++ sonar.sh
@@ -0,0 +1,4 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=KafkaMulesoft \
+  -Dsonar.host.url=https://sonar.florence-consulting.it \
+  -Dsonar.login=sqp_4e017d7fe8cfc924676276a23f78033be5a1099e
